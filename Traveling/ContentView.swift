f0f9a946d85e94fc5bca3684a920efd601c494c8
Guillaume Richard <guitch95@gmail.com>
2026-06-09 12:37:52 +0200

    Initial commit

---
+++ Traveling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Traveling
+//
+//  Created by Guillaume Richard on 09/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Traveling/TravelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelingApp.swift
+//  Traveling
+//
+//  Created by Guillaume Richard on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
