6f11e266753b7bae27c7e54c4b803854c1a3cda1
Guillaume Richard <guitch95@gmail.com>
2026-06-09 15:09:45 +0200

    Added the features for recommendation view

---
+++ Traveling/View/RecommendationView.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationView.swift
+//  Traveling
+//
+//  Created by Guillaume Richard on 09/06/2026.
+//
+
+import SwiftUI
+
+struct RecommendationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendationView()
+}
